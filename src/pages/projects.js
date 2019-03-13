766b4c83b837c7777afa8d12de8bfe50a93f9dbd
Adam Frej <awfrej@googlemail.com>
2019-03-12 21:45:55 -0400

    resolve pagescontainer broken import

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import Layout from 'components/layout';
-import ProjectsContainer from 'components/ProjectsContainer';
+import ProjectsContainer from './components/ProjectsContainer';
 
 const Projects = ({ data }) => (
   <Layout pageTitle={data.projectsJson.title}>
